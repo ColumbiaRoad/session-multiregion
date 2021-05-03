20ed1ed55fe920a4da45be286f28ccbbc7a08b66
Thor Galle <thorgalle@gmail.com>
2021-05-03 08:47:47 +0200

    Adds custom multi region code & integrates it

--- modules/menu-section-multireg.module/module.js
+++ modules/menu-section-multireg.module/module.js
@@ -15,3 +15,49 @@
     });
   });
 })();
+
+// https://codeburst.io/the-only-way-to-detect-touch-with-javascript-7791a3346685
+window.addEventListener(
+  "touchstart",
+  function onFirstTouch() {
+    window.USER_IS_TOUCHING = true;
+    // we only need to know once that a human touched the screen, so we can stop listening now
+    window.removeEventListener("touchstart", onFirstTouch, false);
+  },
+  false
+);
+
+$(document).ready(function () {
+  function selectRegion() {
+    //  $(this) value should be an <a> element related to a region
+    var selectedRegionSlug = $(this).data("region");
+    // show & hide language sets
+    $(".lang_list_class").each(function () {
+      if ($(this).data("region") === selectedRegionSlug) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+  }
+
+  // set up region switch
+  $(".region_list_class a")
+    // for desktop
+    .hover(selectRegion)
+    // for touch
+    .click(function (event) {
+      if (window.USER_IS_TOUCHING) {
+        // don't open the default region on mobile tap
+        event.preventDefault();
+      }
+      selectRegion.call(this);
+    });
+
+  $(".globe_class > .menu-item").mouseenter(function (event) {
+    $(".globe_class").addClass("open");
+    setTimeout(function () {
+      $(".globe_class").removeClass("open");
+    }, 1000);
+  });
+});
